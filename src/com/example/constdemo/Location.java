bf2a0da852934f101c9730ffa9c6423db7b2a23d
Alisha Hayman <alishaagyt@gmail.com>
2014-04-01 01:18:24 -0400

    Added some IntelliJ files (should not affect program in Eclipse). Building Location class.

---
+++ src/com/example/constdemo/Location.java
@@ -0,0 +1,48 @@
+package com.example.constdemo;
+
+import java.util.ArrayList;
+
+/* Use for location queries. */
+public class Location {
+
+    private int minLat;
+    private int maxLat;
+    private ArrayList<Constellation> constellations;
+
+    public Location (int min, int max, ArrayList<Constellation> list)
+    {
+        minLat = min;
+        maxLat = max;
+        constellations = list;
+    }
+
+    public int getMinLat()
+    {
+        return minLat;
+    }
+
+    public int getMaxLat()
+    {
+        return maxLat;
+    }
+
+    public ArrayList<Constellation> getConstellations()
+    {
+        return constellations;
+    }
+
+    public void setMinLat(int min)
+    {
+        minLat = min;
+    }
+
+    public void setMaxLat(int max)
+    {
+        maxLat = max;
+    }
+
+    public void setConstellations(ArrayList<Constellation> list)
+    {
+        constellations = list;
+    }
+}
